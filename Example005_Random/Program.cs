a4c01f1cf857aa66ff97bb8b58f6df8b6f7b13c2
«Andrey_Nord» <spglav@mail.ru>
2022-08-07 02:48:08 +0300

    add Example 005 Внес изменение в Console.Write

--- Example005_Random/Program.cs
+++ Example005_Random/Program.cs
@@ -1,6 +1,9 @@
-﻿int numberA = new Random().Next(1,10); //случайные числа от 1 до 9
-Console.WriteLine(numberA);
-int numberB = new Random().Next(1,10);
-Console.WriteLine(numberB);
+﻿int numberA = new Random().Next(1, 10); //случайные числа от 1 до 9
+int numberB = new Random().Next(1, 10);
 int result = numberA + numberB;
-Console.WriteLine(result);
+Console.Write("Переменная А = ");
+Console.Write(numberA);
+Console.Write(" Переменная B = ");
+Console.WriteLine(numberB);
+Console.Write("Сумма = ");
+Console.Write(numberA + numberB);
