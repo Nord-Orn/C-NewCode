b1f5d5e663b7e1a57a1e81120c43aeb011c791ac
«Andrey_Nord» <spglav@mail.ru>
2022-08-06 11:48:38 +0300

    Example 007 Задача с гирями MAX найти

---
+++ Example007_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 3;
+int b = 6;
+int c = 8;
+int d = 5;
+int e = 1;
+
+int max = 0;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (a > max) max = a;
+
+Console.Write("MAX = ");
+Console.WriteLine(max);
